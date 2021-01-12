132e05a78035f6a7556af735cd0a5f145d65a53c
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-12 14:41:56 -0800

    ADD(pins routes and tests)

--- server/api/pins.js
+++ server/api/pins.js
@@ -6,10 +6,12 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const pins = await Pin.findAll({
-      // where: {
-      //   [Op.ne]: 0,
-      // },
-      attributes: ['name', 'imageUrl', 'price']
+      where: {
+        stock: {
+          [Op.ne]: 0
+        }
+      },
+      attributes: ['id', 'name', 'imageUrl', 'price']
     })
     res.send(pins)
   } catch (error) {
@@ -19,11 +21,51 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const pin = await Pin.findByPk({
+    const pin = await Pin.findAll({
       where: {id: req.params.id},
-      attributes: ['name', 'description', 'imageUrl', 'price', 'rating']
+      attributes: {exclude: ['stock']}
     })
     res.send(pin)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const pin = await Pin.create(req.body)
+    res.send(pin)
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueContraintError') {
+      res.status(401).send('Pin already exists')
+    } else {
+      next(error)
+    }
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const [num, result] = await Pin.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+      returning: true
+    })
+    if (num) res.send(result)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const result = await Pin.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (result) res.sendStatus(204)
   } catch (error) {
     next(error)
   }
