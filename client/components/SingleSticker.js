077bc76e18555b3912f94101cf964dc23d135408
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-12 20:02:18 -0800

    FEAT: SingleSticker view

---
+++ client/components/SingleSticker.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import {getSingleSticker} from '../store/singleSticker'
+
+class SingleSticker extends Component {
+  componentDidMount() {
+    this.fetchSticker(this.props.params.id)
+  }
+
+  render() {
+    const {sticker: {name, description, imageUrl, price, quantity}} = this.props
+
+    return (
+      <div className="single-sticker-container">
+        <div className="single-sticker-content">
+          <div className="image-container">
+            <img src={imageUrl} />
+          </div>
+          <div className="info-container">
+            <h1>{name}</h1>
+            <p>Rating: {rating}</p>
+            <p>{description}</p>
+            <p>{quantity}</p>
+            <h4>{price}</h4>
+            {/* <AddToCart /> need to finish */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapState = state => ({
+  sticker: state.sticker
+})
+
+const mapDispatch = dispatch => ({
+  fetchSticker: id => dispatch(getSingleSticker(id))
+})
+
+export default connect(mapState, mapDispatch)(SingleSticker)
