0ebe2a80705abe8253ca8c52c8228905730cb824
Gabriel Smith <Gabe@Gabes-Laptop.home>
2021-01-13 09:21:14 -0500

    FE: connected redux store

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,12 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import cartReducer from './cart'
+import pinsReducer from './pins'
 
 const reducer = combineReducers({
   user,
-  cart: cartReducer
+  cart: cartReducer,
+  pins: pinsReducer
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
