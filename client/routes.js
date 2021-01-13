a5b270220a75716fb5515bf360c4b6eba2256b94
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-12 20:00:52 -0800

    ADD: route to single sticker

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Cart} from './components'
+import {Login, Signup, UserHome, Cart, SingleSticker} from './components'
 import {me} from './store'
 
 /**
@@ -22,6 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/cart" component={Cart} />
+        <Route path="/stickers/:id" component={SingleSticker} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
