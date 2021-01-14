ab88e0c513aa4aa02e2f331f7353648644a88fec
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-13 19:39:19 -0800

    STYLE: bare bones navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,34 +4,49 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <Link to="/">
-      <h1>PINS&STICKERS</h1>
-    </Link>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">Cart</Link>
-          <Link to="/stickers">Stickers</Link>
-          <Link to="/pins">Pins</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined'
+import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined'
+
+const Navbar = ({handleClick, isLoggedIn}) => {
+  return (
+    <div>
+      <div className="nav-bar-container">
+        <Link to="/">
+          <h1>PINS&STICKERS</h1>
+        </Link>
+        <nav className="nav-bar-routes-container center">
+          <Link className="nav-bar-routes" to="/pins">
+            Pins
+          </Link>
+          <Link className="nav-bar-routes" to="/stickers">
+            Stickers
+          </Link>
+          <Link className="nav-bar-routes" to="/cart">
+            <ShoppingCartOutlinedIcon />
+          </Link>
+          <div className="nav-bar-routes-container center">
+            {isLoggedIn ? (
+              <div className="nav-bar-routes-container center">
+                <Link className="nav-bar-routes" to="/home">
+                  <PersonOutlineOutlinedIcon />
+                </Link>
+                <a className="nav-bar-routes" href="#" onClick={handleClick}>
+                  <ExitToAppOutlinedIcon />
+                </a>
+              </div>
+            ) : (
+              <Link className="nav-bar-routes" to="/login">
+                <PersonOutlineOutlinedIcon />
+              </Link>
+            )}
+          </div>
+        </nav>
+      </div>
+      <hr />
+    </div>
+  )
+}
 
 /**
  * CONTAINER
