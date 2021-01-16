38c367970a524617fde0ad6486dc033928835fb6
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-16 09:57:20 -0800

    FIX: product model and tests

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -9,60 +9,63 @@ describe('Pin routes', () => {
     return db.sync({force: true})
   })
 
-  describe('GET /api/pins/', () => {
-    const data = [{name: 'FSA'}, {name: 'Test', stock: 0}]
+  describe('GET /api/products/', () => {
+    const data = [{name: 'FSA'}, {name: 'Test', stock: 0, category: 'Stickers'}]
 
     beforeEach(() => {
       return Product.bulkCreate(data)
     })
 
-    it('GET /api/pins', async () => {
+    it('GET /api/products', async () => {
       const res = await request(app)
-        .get('/api/pins')
+        .get('/api/products')
         .expect(200)
 
       expect(res.body).to.be.an('array')
       expect(res.body.length).to.be.equal(1)
       expect(res.body[0].name).to.be.equal('FSA')
       expect(res.body[0].imageUrl).to.be.equal(
-        'https://www.flaticon.com/svg/vstatic/svg/891/891448.svg?token=exp=1610479981~hmac=02dd2421d9afc0aadfa973d68ce812bb'
+        'https://www.flaticon.com/svg/vstatic/svg/1274/1274860.svg?token=exp=1610559388~hmac=85e15df3cecccc2b4e65ba64d09a2b97'
       )
-      expect(res.body[0].price).to.be.equal(0)
+      expect(res.body[0].msrp).to.be.equal(1)
+      expect(res.body[0].rating).to.be.equal(0)
+      expect(res.body[0].category).to.be.equal('Pins')
     })
 
-    it('GET /api/pins/:id', async () => {
+    it('GET /api/products/:id', async () => {
       const res = await request(app)
-        .get('/api/pins/2')
+        .get('/api/products/2')
         .expect(200)
 
       expect(res.body.name).to.be.equal('Test')
       expect(res.body.imageUrl).to.be.equal(
-        'https://www.flaticon.com/svg/vstatic/svg/891/891448.svg?token=exp=1610479981~hmac=02dd2421d9afc0aadfa973d68ce812bb'
+        'https://www.flaticon.com/svg/vstatic/svg/1274/1274860.svg?token=exp=1610559388~hmac=85e15df3cecccc2b4e65ba64d09a2b97'
       )
-      expect(res.body.price).to.be.equal(0)
-      expect(res.body.quantity).to.be.equal(0)
+      expect(res.body.msrp).to.be.equal(1)
+      expect(res.body.rating).to.be.equal(0)
+      expect(res.body.category).to.be.equal('Stickers')
     })
   })
 
-  describe('POST /api/pins/', () => {
+  describe('POST /api/products/', () => {
     it('can create a new pin', async () => {
       const res = await request(app)
-        .post('/api/pins')
+        .post('/api/products')
         .send({name: 'Testing create'})
       expect(res.body.name).to.be.equal('Testing create')
     })
   })
 
-  describe('PUT /api/pins/:id', () => {
+  describe('PUT /api/products/:id', () => {
     const data = [{name: 'FSA'}, {name: 'Test', stock: 0}]
 
     beforeEach(() => {
       return Product.bulkCreate(data)
     })
 
-    it('can update a pin', async () => {
+    it('can update a products', async () => {
       const res = await request(app)
-        .put('/api/pins/1')
+        .put('/api/products/1')
         .send({name: 'New Name', description: 'Testing update'})
 
       expect(res.body[0].name).to.be.equal('New Name')
@@ -70,16 +73,16 @@ describe('Pin routes', () => {
     })
   })
 
-  describe('DELETE /api/pins/:id', () => {
+  describe('DELETE /api/products/:id', () => {
     const data = [{name: 'FSA'}, {name: 'Test', stock: 0}]
 
     beforeEach(() => {
       return Product.bulkCreate(data)
     })
 
-    it('can delete a pin', async () => {
+    it('can delete a product', async () => {
       await request(app)
-        .delete('/api/pins/1')
+        .delete('/api/products/1')
         .expect(204)
     })
   })
