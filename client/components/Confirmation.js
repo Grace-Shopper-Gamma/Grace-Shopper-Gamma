83bde2355c9c4207381acf901eb11b0ddc49c949
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-18 21:24:17 -0800

    STYLE: checkout page

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -2,18 +2,34 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getConfirmation} from '../store/checkout'
 
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
+import {green} from '@material-ui/core/colors'
+
 class Confirmation extends Component {
   componentDidMount() {
     this.props.loadConfirmation(this.props.orderId)
   }
 
   render() {
-    const {confirmation} = this.props
-    return <div>This is your confirmation number: {confirmation}</div>
+    const {confirmation, user: {email}} = this.props
+    return (
+      <div className="confirmation-container">
+        <div className="confirmation-header">
+          <CheckCircleOutlineIcon style={{color: green[500]}} />
+          <p> Thank you for your purchase!</p>
+        </div>
+        <hr />
+        <p>Your confirmation number is: {confirmation}</p>
+        <p>A receipt will be sent to: {email}.</p>
+      </div>
+    )
   }
 }
 
-const mapState = state => ({confirmation: state.confirmation})
+const mapState = state => ({
+  confirmation: state.confirmation,
+  user: state.user
+})
 const mapDispatch = dispatch => ({
   loadConfirmation: orderId => dispatch(getConfirmation(orderId))
 })
